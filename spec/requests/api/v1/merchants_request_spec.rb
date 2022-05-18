312b1d6b9e2ae57be43c1fe76d5de25f6bbf91dd
Oscar Santos <oscar303data@gmail.com>
2022-05-17 22:20:25 -0600

    get one merchant by its id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -24,4 +24,23 @@ RSpec.describe 'Merchants API' do
       end
     end
   end
+
+  describe 'get one merchant endpoint' do
+    it 'can get one merchant by its id' do
+      merchant1 = create(:merchant)
+
+      get "/api/v1/merchants/#{merchant1.id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+
+      expect(merchant).to be_a(Hash)
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_a(String)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+  end
 end
